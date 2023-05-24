820ffb390c8733a1fa607d8c8f06ea74779ed9b1
tami2712 <tami.vga@gmail.com>
2023-05-24 03:41:47 -0300

    Agrego el archivo conversionGrados.js

---
+++ PracticoN0/conversionGrados.js
@@ -0,0 +1,13 @@
+// Parte 3
+let Farenheit 
+let Celsius 
+
+// Parte 3a
+Farenheit = 50
+Celsius = (Farenheit - 32)*5/9;
+console.log(Celsius)
+
+// Parte 3b
+Farenheit = 75
+Celsius = (Farenheit - 32)*5/9;
+console.log(Celsius)
