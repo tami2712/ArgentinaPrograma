7114755f130c81a19003c94b8126131e93856406
tami2712 <tami.vga@gmail.com>
2023-05-24 03:43:54 -0300

    Practico N0 finalizado

---
+++ PracticoN0/areaRectangulo.js
@@ -0,0 +1,15 @@
+let base;
+let altura;
+let area;
+
+//Parte 2a
+base = 5
+altura = 2
+area = base * altura
+console.log(area);}
+
+//Parte 2b
+base = 12.5
+altura = 5.3
+area = base * altura;
+console.log(area);
---
+++ PracticoN0/promedio3.js
@@ -0,0 +1,25 @@
+let num1
+let num2
+let num3
+let promedio
+
+//parte 4a
+num1 = 5
+num2 = 7
+num3 = 9
+promedio = (num1 + num2 + num3)/3;
+console.log(promedio)
+
+//parte 4b
+num1 = -2
+num2 = -3
+num3 = -8
+promedio = (num1 + num2 + num3)/3;
+console.log(promedio)
+
+//parte 4c
+num1 = -1.2
+num2 = 5.3
+num3 = -3.3
+promedio = (num1 + num2 + num3)/3;
+console.log(promedio)
