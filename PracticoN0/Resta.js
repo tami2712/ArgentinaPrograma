710a58cffa10b991e16dc496ae1ec16574af2fa3
tami2712 <tami.vga@gmail.com>
2023-05-24 03:33:50 -0300

    intento commit

---
+++ PracticoN0/Resta.js
@@ -0,0 +1,5 @@
+// Parte 1a
+let minuendo = 10
+let sustraendo = 2
+let resta = minuendo - sustraendo;
+console.log (resta);
---
+++ PracticoN0/multiplicacion.js
@@ -0,0 +1,6 @@
+// Parte 1b
+let num1 = 2
+let num2 = 17
+let num3 = 6
+let multiplicacion = num1 * num2 * num3
+console.log(multiplicacion);
